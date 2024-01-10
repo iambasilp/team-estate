1da1097b1aa51f297c704178ae38f26ddc788ca0
iambasilp <basilxg321@gmail.com>
2024-01-10 12:43:04 +0530

    Implement user sign-up functionality with form handling, validation, and error display

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,40 +1,87 @@
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
-    <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7 text-slate-700">
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7 text-slate-700'>
         Sign Up
       </h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
-          type="text"
-          placeholder="Username"
-          className="border p-3 rounded-lg focus:outline-none focus:border-blue-500"
-          id="username"
+          type='text'
+          placeholder='Username'
+          className='border p-3 rounded-lg focus:outline-none focus:border-blue-500'
+          id='username'
+          onChange={handleChange}
         />
         <input
-          type="email"
-          placeholder="Email"
-          className="border p-3 rounded-lg focus:outline-none focus:border-blue-500"
-          id="email"
+          type='email'
+          placeholder='Email'
+          className='border p-3 rounded-lg focus:outline-none focus:border-blue-500'
+          id='email'
+          onChange={handleChange}
         />
         <input
-          type="password"
-          placeholder="Password"
-          className="border p-3 rounded-lg focus:outline-none focus:border-blue-500"
-          id="password"
+          type='password'
+          placeholder='Password'
+          className='border p-3 rounded-lg focus:outline-none focus:border-blue-500'
+          id='password'
+          onChange={handleChange}
         />
-        <button className="bg-blue-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign up
+        <button
+          disabled={loading}
+          className='bg-blue-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign Up'}
         </button>
       </form>
-      <div className="flex gap-2 mt-5">
-        <p className="text-slate-700">Have an account?</p>
-        <Link to={"/sign-in"}>
-          <span className="text-blue-700">Sign in</span>
+      <div className='flex gap-2 mt-5'>
+        <p className='text-slate-700'>Have an account?</p>
+        <Link to={'/sign-in'}>
+          <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   );
 };
