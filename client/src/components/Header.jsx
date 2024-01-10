196844eabcb10b9bbddecc734ca631d39daa61bf
iambasilp <basilxg321@gmail.com>
2024-01-10 12:25:26 +0530

    Update header styling for a better look

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,37 +3,36 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className='bg-slate-200 shadow-md'>
+    <header className='bg-blue-700 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
-          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
-            <span className='text-slate-700'>Estate</span>
+          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap text-white'>
+            <span className='text-yellow-300'>Kerala</span>
+            <span className='text-white'>Real Estate</span>
           </h1>
         </Link>
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+        <form className='bg-blue-500 p-3 rounded-lg flex items-center'>
           <input
             type='text'
             placeholder='Search...'
-            className='bg-transparent focus:outline-none w-24 sm:w-64'
+            className='bg-transparent focus:outline-none w-24 sm:w-64 text-white'
           />
-          <FaSearch className='text-slate-600' />
+          <FaSearch className='text-white' />
         </form>
         <ul className='flex gap-4'>
           <Link to='/'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
+            <li className='hidden sm:inline text-white hover:underline'>
               Home
             </li>
           </Link>
           <Link to='/about'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
+            <li className='hidden sm:inline text-white hover:underline'>
               About
             </li>
           </Link>
           <Link to='/sign-in'>
-            <li className=' text-slate-700 hover:underline'> Sign in</li>
+            <li className='text-white hover:underline'> Sign in</li>
           </Link>
-
         </ul>
       </div>
     </header>
