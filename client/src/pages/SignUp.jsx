2195889ffeebed6fbecff63b085713205d3c5cb2
iambasilp <basilxg321@gmail.com>
2024-01-10 12:26:40 +0530

    Enhance SignUp page with improved design and styling

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,38 +1,42 @@
 import { Link } from "react-router-dom";
 
-export default function SignUp() {
+const SignUp = () => {
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <h1 className="text-3xl text-center font-semibold my-7 text-slate-700">
+        Sign Up
+      </h1>
       <form className="flex flex-col gap-4">
         <input
           type="text"
-          placeholder="username"
-          className="border p-3 rounded-lg"
+          placeholder="Username"
+          className="border p-3 rounded-lg focus:outline-none focus:border-blue-500"
           id="username"
         />
         <input
           type="email"
-          placeholder="email"
-          className="border p-3 rounded-lg"
+          placeholder="Email"
+          className="border p-3 rounded-lg focus:outline-none focus:border-blue-500"
           id="email"
         />
         <input
           type="password"
-          placeholder="password"
-          className="border p-3 rounded-lg"
+          placeholder="Password"
+          className="border p-3 rounded-lg focus:outline-none focus:border-blue-500"
           id="password"
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+        <button className="bg-blue-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
           Sign up
         </button>
       </form>
       <div className="flex gap-2 mt-5">
-        <p>Have an account?</p>
+        <p className="text-slate-700">Have an account?</p>
         <Link to={"/sign-in"}>
           <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
     </div>
   );
-}
+};
+
+export default SignUp;
