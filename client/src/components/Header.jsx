f8c88874bb3f952b9f23228fb8dbe51236bcd7da
iambasilp <basilxg321@gmail.com>
2024-01-11 21:19:00 +0530

    Enhance Header Design and Implement Private Profile Page Access

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,10 @@
 import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className='bg-blue-700 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -30,8 +33,12 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to='/sign-in'>
-            <li className='text-white hover:underline'> Sign in</li>
+          <Link to='/profile'>
+            {currentUser ? (
+              <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile' />
+            ) : (
+              <li className=' text-white hover:underline'> Sign in</li>
+            )}
           </Link>
         </ul>
       </div>
