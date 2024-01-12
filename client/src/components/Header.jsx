bc482fb3080d991c7301e7de370dfc5cd6df9175
iambasilp <basilxg321@gmail.com>
2024-01-12 12:18:47 +0530

    Updated header styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,34 +2,33 @@ import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
   return (
-    <header className='bg-blue-700 shadow-md'>
+    <header className='bg-blue-900 text-white shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
-          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap text-white'>
-            <span className='text-yellow-300'>Kerala</span>
-            <span className='text-white'>Real Estate</span>
+          <h1 className='font-bold text-lg sm:text-xl flex flex-wrap'>
+            <span className='text-blue-300'>Basil</span>
+            <span className='text-blue-100'>Estate</span>
           </h1>
         </Link>
-        <form className='bg-blue-500 p-3 rounded-lg flex items-center'>
+        <form className='bg-blue-800 p-3 rounded-lg flex items-center'>
           <input
             type='text'
             placeholder='Search...'
             className='bg-transparent focus:outline-none w-24 sm:w-64 text-white'
           />
-          <FaSearch className='text-white' />
+          <FaSearch className='text-blue-100' />
         </form>
         <ul className='flex gap-4'>
           <Link to='/'>
-            <li className='hidden sm:inline text-white hover:underline'>
+            <li className='hidden sm:inline text-blue-100 hover:underline'>
               Home
             </li>
           </Link>
           <Link to='/about'>
-            <li className='hidden sm:inline text-white hover:underline'>
+            <li className='hidden sm:inline text-blue-100 hover:underline'>
               About
             </li>
           </Link>
@@ -37,7 +36,7 @@ export default function Header() {
             {currentUser ? (
               <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile' />
             ) : (
-              <li className=' text-white hover:underline'> Sign in</li>
+              <li className='text-blue-100 hover:underline'>Sign in</li>
             )}
           </Link>
         </ul>
