f4e51b0bf4a55d9abadcdd31a16842cbbb452213
Basil Pulikuth <basilxg321@gmail.com>
2024-01-15 19:49:04 +0530

    Add Sign Out functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import bcryptjs from 'bcryptjs';
-import User from '../models/user.model.js';
+import User from "../modules/user.module.js";
 import { errorHandler } from '../utils/error.js';
 
 export const test = (req, res) => {
