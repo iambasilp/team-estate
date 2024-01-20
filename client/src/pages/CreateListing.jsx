baec13b2e2d52800ac373ed680baf12330ac8d78
ANAS <anaspappadan@gmail.com>
2024-01-20 13:41:24 +0530

    remove log statements

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -31,7 +31,6 @@ export default function CreateListing() {
   const [uploading, setUploading] = useState(false);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
-  console.log(formData);
   const handleImageSubmit = (e) => {
     if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
       setUploading(true);
