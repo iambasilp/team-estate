b511241091de697b675fa90a4142304aabe79a97
nishaj0 <njnishaj0@gmail.com>
2024-01-22 13:03:07 +0530

    refactor(server): move DB connection code to connectDB.js
    and server only start when it connected to DB

---
+++ api/config/connectDB.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+     try {
+          mongoose.connect(process.env.MONGO);
+     } catch (error) {
+          console.log(error);
+     }
+};
+
+export default connectDB;
