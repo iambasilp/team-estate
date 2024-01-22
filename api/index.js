edd5112416987b9f40a5038e641d8f2722edee55
nishaj0 <njnishaj0@gmail.com>
2024-01-22 13:03:07 +0530

    refactor(server): move DB connection code to connectDB.js
    and server only start when it connected to DB

--- api/index.js
+++ api/index.js
@@ -1,23 +1,18 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
-import listingRouter from './routes/listing.route.js';
-import cookieParser from 'cookie-parser';
-import path from 'path';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
+import cookieParser from "cookie-parser";
+import path from "path";
+import connectDB from "./config/connectDB.js";
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    console.log('Connected to MongoDB!');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// ? Connect to DB
+connectDB();
 
-  const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -25,27 +20,28 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+     res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/listing', listingRouter);
-
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+// ? once connected to DB, start server
+mongoose.connection.once("open", () => {
+     console.log("connected to DB");
+     app.listen(3000, () => console.log("Server is running on port 3000!"));
+});
 
 app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
-  return res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
-  });
+     const statusCode = err.statusCode || 500;
+     const message = err.message || "Internal Server Error";
+     return res.status(statusCode).json({
+          success: false,
+          statusCode,
+          message,
+     });
 });
