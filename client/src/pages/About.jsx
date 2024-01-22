6eb85600a763d38f1e9f59bb97ce9d97a4a8c760
ANAS <anaspappadan@gmail.com>
2024-01-22 21:23:57 +0530

    color theme and responsiveness updates

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
       name: 'BASIL',
       image:
         'https://www.rugbyafrique.com/wp-content/uploads/2018/02/team-member.jpg',
-      twitter: '#',
+      mail: '#',
       facebook: '#',
       instagram: '#',
       linkedin: '#',
@@ -17,7 +17,7 @@ export default function About() {
       name: 'NISHAJ',
       image:
         'https://www.rugbyafrique.com/wp-content/uploads/2018/02/team-member.jpg',
-      twitter: '#',
+      mail: '#',
       facebook: '#',
       instagram: '#',
       linkedin: '#',
@@ -26,7 +26,7 @@ export default function About() {
       name: 'SHUHAIB',
       image:
         'https://www.rugbyafrique.com/wp-content/uploads/2018/02/team-member.jpg',
-      twitter: '#',
+      mail: '#',
       facebook: '#',
       instagram: '#',
       linkedin: '#',
@@ -35,7 +35,7 @@ export default function About() {
       name: 'ANAS',
       image:
         'https://www.rugbyafrique.com/wp-content/uploads/2018/02/team-member.jpg',
-      twitter: '#',
+      mail: '#',
       facebook: '#',
       instagram: '#',
       linkedin: '#',
@@ -82,7 +82,7 @@ export default function About() {
           </div>
         </div>
       </div>
-      <div className='mt-32 rounded-lg '>
+      <div className='mt-32 rounded-lg bg-dimBlue'>
         <section className='max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12'>
           <div className='text-center pb-12'>
             <h1 className='font-bold text-xl md:text-4xl lg:text-4xl font-heading text-gray-900 pb-10'>
@@ -100,11 +100,11 @@ export default function About() {
               user authentication features.
             </p>
           </div>
-          <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-16'>
+          <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-10'>
             {teamMembers.map((member) => (
               <div
                 key={member.name}
-                className='w- bg-dimBlue rounded-lg pt-2 sm:pt-12 flex flex-col justify-center items-center'
+                className=' bg-dimBlue rounded-lg pt-2 sm:pt-12 flex flex-col justify-center items-center'
               >
                 <img
                   className='object-center object-cover rounded-full h-24 w-24 sm:mb-5'
@@ -120,8 +120,9 @@ export default function About() {
                 <div className='mt-2 mb-2 sm:mb-5 md:mb-5 space-x-2 flex '>
                   <a
                     className='hover:text-red-600'
-                    aria-label='Twitter link'
-                    href={member.twitter}
+                    aria-label='Mail link'
+                    title='Mail'
+                    href={member.mail}
                   >
                     <svg
                       xmlns='http://www.w3.org/2000/svg'
@@ -139,6 +140,7 @@ export default function About() {
                   <a
                     className='hover:text-blue-900'
                     aria-label='Facebook link'
+                    title='Facebook'
                     href={member.facebook}
                   >
                     <svg
@@ -157,6 +159,7 @@ export default function About() {
                   <a
                     className='hover:text-pink-600'
                     aria-label='Instagram link'
+                    title='Instagram'
                     href={member.instagram}
                   >
                     <svg
@@ -183,6 +186,7 @@ export default function About() {
                   <a
                     className='hover:text-blue-700'
                     aria-label='Linkedin link'
+                    title='Linkedin'
                     href={member.linkedin}
                   >
                     <svg
