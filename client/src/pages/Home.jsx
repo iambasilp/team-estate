cde1db7db39be090686e8c0d9fee743efcb87a74
iambasilp <basilxg321@gmail.com>
2024-01-24 08:01:35 +0530

    Layout fixed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -34,7 +34,7 @@ export default function Home() {
     <div>
       {/* top */}
       <section
-        className={`app__home flex flex-col md:flex-row sm:py-32 py-24 `}
+        className={`app__home flex flex-col md:flex-row sm:py-32  `}
       >
         <div
           className={`app__home-info flex-1 flex-col ${styles.flexStart} mt-10 ${styles.paddingX}`}
