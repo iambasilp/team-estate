137feb9c6ea220447d82c656ab7ffe8ce466ba57
iambasilp <basilxg321@gmail.com>
2024-01-24 15:38:29 +0530

    service page updated

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -1,158 +1,55 @@
 import React from "react";
+import styles from "../style";
+import serviceOne from "../assets/service_1.svg";
+import serviceTwo from "../assets/service_2.svg";
+import serviceThree from "../assets/service_3.svg";
 
 export default function services() {
    return (
-      <div className="relative overflow-hidden bg-primary pt-16 pb-32 space-y-24">
-         <div className="relative">
-            <h1 className="text-gray-900 text-5xl font-semibold mb-10 py-10  px-6 text-center">Our Services</h1>
-            <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2 lg:gap-24 lg:px-8 ">
-               <div className="mx-auto max-w-xl px-6 lg:mx-0 lg:max-w-none lg:py-16 lg:px-0 ">
-                  <div>
-                     <div className="mt-6">
-                        <h2 className="text-3xl font-bold tracking-tight text-slate-950">
-                           Comprehensive Property Listings:
-                        </h2>
-                        <p className="mt-4 text-lg text-gray-700">
-                           Explore a vast array of residential and commercial properties through our comprehensive
-                           listings. We offer detailed information, high-quality images, and advanced search filters to
-                           help you find the perfect property that suits your preferences and requirements.
-                        </p>
-                        <div className="mt-6">
-                           <a
-                              className="inline-flex rounded-lg text-white bg-blue-700 px-4 py-1.5 text-base font-semibold leading-7  shadow-sm ring-1 ring-blue-700 hover:bg-blue-700 hover:ring-blue-700"
-                              href="/properties"
-                           >
-                              Learn More
-                           </a>
-                        </div>
-                     </div>
-                  </div>
+      <div className="relative py-16 section sm:my-28 my-20 ">
+         <div className="container relative m-auto px-6 text-gray-500 md:px-12">
+            <div className="grid gap-6 md:mx-auto md:w-8/12 lg:w-full lg:grid-cols-3">
+               <div className="group space-y-6 border border-gray-100 dark:border-gray-700 rounded-3xl bg-white dark:bg-gray-800 px-8 py-12 text-center shadow-2xl shadow-gray-600/10 dark:shadow-none">
+                  <img className="mx-auto w-24" src={serviceOne} alt="illustration" loading="lazy" />
+                  <h3 className="text-2xl font-semibold text-gray-800 dark:text-white">Web development</h3>
+                  <p>
+                     Obcaecati, quam? Eligendi, nulla numquam natus laborum porro at cum, consectetur ullam tempora ipsa
+                     iste officia sed officiis! Incidunt ea animi officiis.
+                  </p>
+                  <a
+                     href="#"
+                     className="relative mx-auto flex h-10 w-10 items-center justify-center before:absolute before:inset-0 before:rounded-full before:border before:border-gray-100 dark:before:border-gray-600 before:transition before:duration-300 group-hover:before:scale-125"
+                  >
+                     <span className="text-primary">→</span>
+                  </a>
                </div>
-               <div className="mt-12 sm:mt-16 lg:mt-0">
-                  <div className="-mr-48 pl-6 md:-mr-16 lg:relative lg:m-0 lg:h-full lg:px-0">
-                     <img
-                        loading="lazy"
-                        width="647"
-                        height="486"
-                        className="w-full rounded-xl shadow-2xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                        src="https://img.freepik.com/free-photo/hand-presenting-model-house-home-loan-campaign_53876-104970.jpg?w=996&t=st=1706078252~exp=1706078852~hmac=579608b1e04f293aa4bf0f82c065bdc1c5cb5230f859c2447293b2690bc2b8f9"
-                     />
-                  </div>
+               <div className="group space-y-6 border border-gray-100 dark:border-gray-700 rounded-3xl bg-white dark:bg-gray-800 px-8 py-12 text-center shadow-2xl shadow-gray-600/10 dark:shadow-none">
+                  <img className="mx-auto w-32" src={serviceTwo} alt="illustration" loading="lazy" />
+                  <h3 className="text-2xl font-semibold text-gray-800 dark:text-white">Seo</h3>
+                  <p>
+                     Obcaecati, quam? Eligendi, nulla numquam natus laborum porro at cum, consectetur ullam tempora ipsa
+                     iste officia sed officiis! Incidunt ea animi officiis.
+                  </p>
+                  <a
+                     href="#"
+                     className="relative mx-auto flex h-10 w-10 items-center justify-center before:absolute before:inset-0 before:rounded-full before:border before:border-gray-100 dark:before:border-gray-600 before:transition before:duration-300 group-hover:before:scale-125"
+                  >
+                     <span className="text-primary">→</span>
+                  </a>
                </div>
-            </div>
-         </div>
-
-         <div className="relative">
-            <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2 lg:gap-24 lg:px-8 ">
-               <div className="mx-auto max-w-xl px-6 lg:mx-0 lg:max-w-none lg:py-16 lg:px-0 lg:col-start-2">
-                  <div>
-                     <div className="mt-6">
-                        <h2 className="text-3xl font-bold tracking-tight text-slate-950">
-                           AI-Powered Property Discovery:
-                        </h2>
-                        <p className="mt-4 text-lg text-gray-700">
-                           Experience a cutting-edge property search with our AI-powered functionality. Our system
-                           employs advanced algorithms to understand your preferences and deliver highly relevant
-                           property listings. Say goodbye to traditional searches and let artificial intelligence
-                           streamline your property discovery process.
-                        </p>
-                        <div className="mt-6">
-                           <a
-                              className="inline-flex rounded-lg text-white bg-blue-700 px-4 py-1.5 text-base font-semibold leading-7 shadow-sm ring-1 ring-blue-700 hover:bg-blue-700 hover:ring-blue-700"
-                              href="/search"
-                           >
-                              Learn More
-                           </a>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-               <div className="mt-12 sm:mt-16 lg:mt-0">
-                  <div className="-ml-48 pr-6 md:-ml-16 lg:relative lg:m-0 lg:h-full lg:px-0">
-                     <img
-                        alt="Inbox user interface"
-                        loading="lazy"
-                        width="647"
-                        height="486"
-                        className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-                        src="https://img.freepik.com/free-vector/robot-doing-repeatable-tasks-with-lot-folders-magnifier-robotic-process-automation-service-robots-profit-automated-processing-concept_335657-2111.jpg?w=996&t=st=1706078876~exp=1706079476~hmac=ada88a5a14560dbbfd40b534d97a0601c624fef26429414e47dab424450e38ce"
-                     />
-                  </div>
-               </div>
-            </div>
-         </div>
-
-         <div className="relative">
-            <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2 lg:gap-24 lg:px-8 ">
-               <div className="mx-auto max-w-xl px-6 lg:mx-0 lg:max-w-none lg:py-16 lg:px-0 ">
-                  <div>
-                     <div className="mt-6">
-                        <h2 className="text-3xl font-bold tracking-tight text-slate-950">High quality Images:</h2>
-                        <p className="mt-4 text-lg text-gray-700">
-                           Immerse yourself in the world of real estate with our high quality presentations.helping you
-                           make more confident decisions from the comfort of your home.
-                        </p>
-                        <div className="mt-6">
-                           <a
-                              className="inline-flex rounded-lg text-white bg-blue-700 px-4 py-1.5 text-base font-semibold leading-7 shadow-sm ring-1 ring-blue-700 hover:bg-blue-700 hover:ring-blue-700"
-                              href="/properties"
-                           >
-                              Learn More
-                           </a>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-               <div className="mt-12 sm:mt-16 lg:mt-0">
-                  <div className="-mr-48 pl-6 md:-mr-16 lg:relative lg:m-0 lg:h-full lg:px-0">
-                     <img
-                        loading="lazy"
-                        width="650"
-                        height="486"
-                        className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-                        src="https://img.freepik.com/free-photo/modern-residential-district-with-green-roof-balcony-generated-by-ai_188544-10276.jpg?w=1060&t=st=1706078422~exp=1706079022~hmac=d39601314a34fc28f4d0b394fdfa7f2a8630bdf15032bfaf033637fcac3dfc9f"
-                     />
-                  </div>
-               </div>
-            </div>
-         </div>
-         <div className="relative">
-            <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2 lg:gap-24 lg:px-8 ">
-               <div className="mx-auto max-w-xl px-6 lg:mx-0 lg:max-w-none lg:py-16 lg:px-0 lg:col-start-2">
-                  <div>
-                     <div className="mt-6">
-                        <h2 className="text-3xl font-bold tracking-tight text-slate-950">
-                           Responsive Customer Support:
-                        </h2>
-                        <p className="mt-4 text-lg text-gray-700">
-                           Our commitment goes beyond property listings. Experience top-notch customer support with our
-                           responsive team. Whether you have questions about a listing, need assistance navigating the
-                           website, or seek guidance on real estate matters, our dedicated support team is here for you.
-                           Reach out through live chat, email, or phone, and let us ensure a smooth and enjoyable
-                           experience throughout your real estate journey. Your satisfaction is our priority.
-                        </p>
-                        <div className="mt-6">
-                           <a
-                              className="inline-flex rounded-lg text-white bg-blue-700 px-4 py-1.5 text-base font-semibold leading-7 shadow-sm ring-1 ring-blue-700 hover:bg-blue-700 hover:ring-blue-700"
-                              href="/contact"
-                           >
-                              Learn More
-                           </a>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-               <div className="mt-12 sm:mt-16 lg:mt-0">
-                  <div className="-ml-48 pr-6 md:-ml-16 lg:relative lg:m-0 lg:h-full lg:px-0">
-                     <img
-                        alt="Inbox user interface"
-                        loading="lazy"
-                        width="647"
-                        height="486"
-                        className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-                        src="https://img.freepik.com/free-vector/organic-flat-customer-support-illustration_23-2148899174.jpg?w=996&t=st=1706079405~exp=1706080005~hmac=ff847a60d72e5d0c849e9d8fdbe0a3bb27445f53f62620a79166876392cdaef3"
-                     />
-                  </div>
+               <div className="group space-y-6 border border-gray-100 dark:border-gray-700 rounded-3xl bg-white dark:bg-gray-800 px-8 py-12 text-center shadow-2xl shadow-gray-600/10 dark:shadow-none">
+                  <img className="mx-auto w-24" src={serviceThree} alt="illustration" loading="lazy" />
+                  <h3 className="text-2xl font-semibold text-gray-800 dark:text-white">Web Design</h3>
+                  <p>
+                     Obcaecati, quam? Eligendi, nulla numquam natus laborum porro at cum, consectetur ullam tempora ipsa
+                     iste officia sed officiis! Incidunt ea animi officiis.
+                  </p>
+                  <a
+                     href="#"
+                     className="relative mx-auto flex h-10 w-10 items-center justify-center before:absolute before:inset-0 before:rounded-full before:border before:border-gray-100 dark:before:border-gray-600 before:transition before:duration-300 group-hover:before:scale-125"
+                  >
+                     <span className="text-primary">→</span>
+                  </a>
                </div>
             </div>
          </div>
