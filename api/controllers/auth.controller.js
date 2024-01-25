496b73d04c391fde4f798aead044cf7d54075d83
nishaj0 <njnishaj0@gmail.com>
2024-01-25 14:56:14 +0530

    refactor(api): updated jwt sign
    added user role as value to jwt sign

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -22,7 +22,9 @@ export const signin = async (req, res, next) => {
       if (!validUser) return next(errorHandler(404, "User not found!"));
       const validPassword = bcryptjs.compareSync(password, validUser.password);
       if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
-      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+      // ? create token with user id and user role
+      const token = jwt.sign({ id: validUser._id, role: validUser.role }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = validUser._doc;
       res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest);
    } catch (error) {
@@ -34,7 +36,7 @@ export const google = async (req, res, next) => {
    try {
       const user = await User.findOne({ email: req.body.email });
       if (user) {
-         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+         const token = jwt.sign({ id: user._id, role: validUser.role }, process.env.JWT_SECRET);
          const { password: pass, ...rest } = user._doc;
          res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest);
       } else {
@@ -47,7 +49,7 @@ export const google = async (req, res, next) => {
             avatar: req.body.photo,
          });
          await newUser.save();
-         const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+         const token = jwt.sign({ id: newUser._id, role: validUser.role }, process.env.JWT_SECRET);
          const { password: pass, ...rest } = newUser._doc;
          res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest);
       }
