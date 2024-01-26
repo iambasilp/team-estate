a45d62d2cdb62efa60abba49d0d8f0528ee93ac1
iambasilp <basilxg321@gmail.com>
2024-01-26 15:49:00 +0530

    developer image integrated

--- client/src/pages/Team/Team.jsx
+++ client/src/pages/Team/Team.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "../../style";
+import BasilDev from '../../assets/developer/basil.png'
 export default function About() {
    return (
       <div className={`${styles.paddingY} mt-5`}>
@@ -26,7 +27,7 @@ export default function About() {
                   <div className="group relative rounded-lg  space-y-6 overflow-hidden">
                      <img
                         className="mx-auto h-[26rem] w-full grayscale object-cover object-top ransition duration-500 group-hover:scale-105 group-hover:grayscale-0"
-                        src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww"
+                        src={BasilDev}
                         alt="woman"
                         loading="lazy"
                         width={640}
