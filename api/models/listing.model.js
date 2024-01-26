d185bf1ce862648b1e21418a9f50cf12d458793d
ANAS <anaspappadan@gmail.com>
2024-01-26 19:50:02 +0530

    Add locationUrl field to listing model

--- api/models/listing.model.js
+++ api/models/listing.model.js
@@ -54,6 +54,10 @@ const listingSchema = new mongoose.Schema(
          type: String,
          required: true,
       },
+      locationUrl: {
+         type: String,
+         required: false,
+      },
    },
    { timestamps: true },
 );
