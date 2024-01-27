03df2f3f886044165e4d65c372dcb6ab7bbe4fc8
nishaj0 <njnishaj0@gmail.com>
2024-01-27 14:49:53 +0530

    refactor(admin-panel): removed searchUser function and add the functionality to getUsers function
    jointed two function to reduce complexity in routing

--- api/controllers/admin.controller.js
+++ api/controllers/admin.controller.js
@@ -1,30 +1,25 @@
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 
-// ? get all users
+// ? get all users or search for users by name or email
 const getUsers = async (req, res, next) => {
-   try {
-      const users = await User.find();
-      res.status(200).json(users);
-   } catch (error) {
-      next(error);
-   }
-};
-
-// ? search users by there name or email
-const findUsers = async (req, res, next) => {
    const { keyword } = req.query;
-   if (!keyword) return next(errorHandler(400, "keyword required"));
-
    try {
-      // ? if the keyword matches the username or email, return the users
-      const users = await User.find({
-         $or: [{ username: { $regex: new RegExp(keyword, "i") } }, { email: { $regex: new RegExp(keyword, "i") } }],
-      })
-         .select({ _id: 1, username: 1, email: 1, role: 1, avatar: 1, createdAt: 1 })
-         .sort({ createdAt: -1 });
+      // ? if no keyword is provided, return all users
+      if (!keyword || keyword === "") {
+         const users = await User.find();
+         res.status(200).json(users);
 
-      res.status(200).json(users);
+         // ? if keyword is provided, search for users by name or email
+      } else {
+         const users = await User.find({
+            $or: [{ username: { $regex: new RegExp(keyword, "i") } }, { email: { $regex: new RegExp(keyword, "i") } }],
+         })
+            .select({ _id: 1, username: 1, email: 1, role: 1, avatar: 1, createdAt: 1 })
+            .sort({ createdAt: -1 });
+
+         res.status(200).json(users);
+      }
    } catch (error) {
       next(error);
    }
@@ -50,4 +45,4 @@ const deleteUser = async (req, res, next) => {
    }
 };
 
-export { getUsers, findUsers, deleteUser };
+export { getUsers, deleteUser };
--- api/routes/admin.route.js
+++ api/routes/admin.route.js
@@ -1,12 +1,11 @@
 import express from "express";
-import { getUsers, findUsers, deleteUser } from "../controllers/admin.controller.js";
+import { getUsers, deleteUser } from "../controllers/admin.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import verifyRoles from "../middleware/verifyRoles.js";
 
 const router = express.Router();
 
 router.get("/users", verifyToken, verifyRoles("admin"), getUsers);
-router.get("/users/search", verifyToken, verifyRoles("admin"), findUsers);
 router.delete("/users/:id", verifyToken, verifyRoles("admin"), deleteUser);
 
 export default router;
