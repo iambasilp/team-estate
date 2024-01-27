44a67147fdd2c9f4b69fa4046be3d78270204f95
nishaj0 <njnishaj0@gmail.com>
2024-01-27 13:06:03 +0530

    feat(admin-panel): add search user functionality

--- api/controllers/admin.controller.js
+++ api/controllers/admin.controller.js
@@ -11,4 +11,23 @@ const getUsers = async (req, res, next) => {
    }
 };
 
-export { getUsers };
+// ? search users by there name or email
+const findUsers = async (req, res, next) => {
+   const { keyword } = req.query;
+   if (!keyword) return next(errorHandler(400, "keyword required"));
+
+   try {
+      // ? if the keyword matches the username or email, return the users
+      const users = await User.find({
+         $or: [{ username: { $regex: new RegExp(keyword, "i") } }, { email: { $regex: new RegExp(keyword, "i") } }],
+      })
+         .select({ _id: 1, username: 1, email: 1, role: 1, avatar: 1, createdAt: 1 })
+         .sort({ createdAt: -1 });
+
+      res.status(200).json(users);
+   } catch (error) {
+      next(error);
+   }
+};
+
+export { getUsers, findUsers };
--- api/routes/admin.route.js
+++ api/routes/admin.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getUsers } from "../controllers/admin.controller.js";
+import { getUsers, findUsers } from "../controllers/admin.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import verifyRoles from "../middleware/verifyRoles.js";
 
 const router = express.Router();
 
 router.get("/users", verifyToken, verifyRoles("admin"), getUsers);
+router.get("/users/search", verifyToken, verifyRoles("admin"), findUsers);
 
 export default router;
