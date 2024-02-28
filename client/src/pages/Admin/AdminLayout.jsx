454c64cb5e72e5dadf92a9288d450640d763debc
nishaj0 <njnishaj0@gmail.com>
2024-02-28 08:14:15 +0530

    feat(admin-panel): make admin page protected
    when the nonlogined or user navigate to "/admin" its shows page not found

--- client/src/pages/Admin/AdminLayout.jsx
+++ client/src/pages/Admin/AdminLayout.jsx
@@ -1,16 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Sidebar from "./admin.containers/Sidebar";
 
 function AdminLayout() {
+   const user = useSelector((state) => state.user);
    return (
-      <div className="flex">
-         <div className="fixed h-full w-64">
-            <Sidebar />
-         </div>
-         <div className="flex-grow ml-64 max-w-[calc(100vw-16rem)]">
-            <Outlet />
-         </div>
-      </div>
+      <>
+         {user?.currentUser?.role === "admin" ? (
+            <div className="flex">
+               <div className="fixed h-full w-64">
+                  <Sidebar />
+               </div>
+               <div className="flex-grow ml-64 max-w-[calc(100vw-16rem)]">
+                  <Outlet />
+               </div>
+            </div>
+         ) : (
+            <Navigate to="*" />
+         )}
+      </>
    );
 }
 
