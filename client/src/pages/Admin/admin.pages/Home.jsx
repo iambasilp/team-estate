9abea8e74eb4940f708127341b087ec396121a39
nishaj0 <njnishaj0@gmail.com>
2024-03-17 18:26:04 +0530

    refactor(admin-panel): add new data charts

--- client/src/pages/Admin/admin.pages/Home.jsx
+++ client/src/pages/Admin/admin.pages/Home.jsx
@@ -9,22 +9,41 @@ function Home() {
             Welcome <span className="font-semibold">{currentUser.username}</span>!
          </h1>
          <h3 className="mt-4 text-xl font-semibold">User Details</h3>
-         <ChartIFrame
+         {/* <ChartIFrame
             src={
                "https://charts.mongodb.com/charts-real-estate-clg-project-ygvkc/embed/charts?id=65f30e34-35e9-4f1e-895d-d999539c8e3e&maxDataAge=3600&theme=light&autoRefresh=true"
+            }
+         /> */}
+         {/* new user chart */}
+         <ChartIFrame
+            src={
+               "https://charts.mongodb.com/charts-team-estate-svehs/embed/charts?id=65f6d286-ee5b-416f-8b6a-e46c641d8b7c&maxDataAge=3600&theme=light&autoRefresh=true"
             }
          />
 
          <h3 className="mt-12 text-xl font-semibold">User Details</h3>
-         <ChartIFrame
+         {/* <ChartIFrame
             src={
                "https://charts.mongodb.com/charts-real-estate-clg-project-ygvkc/embed/charts?id=65f319d6-2677-4eae-8f79-634e9c1fce16&maxDataAge=3600&theme=light&autoRefresh=true"
             }
-         />
+         /> */}
+         {/* new listing chart */}
          <ChartIFrame
+            src={
+               "https://charts.mongodb.com/charts-team-estate-svehs/embed/charts?id=65f6e088-bcf4-4c91-8a7c-04fc210dd1a3&maxDataAge=3600&theme=light&autoRefresh=true"
+            }
+         />
+         {/* <ChartIFrame
             marginTop={"4rem"}
             src={
                "https://charts.mongodb.com/charts-real-estate-clg-project-ygvkc/embed/charts?id=65f31d55-5f0c-4392-85a9-c715c60ed344&maxDataAge=3600&theme=light&autoRefresh=true"
+            }
+         /> */}
+         {/* new listing chart */}
+         <ChartIFrame
+            marginTop={"4rem"}
+            src={
+               "https://charts.mongodb.com/charts-team-estate-svehs/embed/charts?id=65f6e0ea-1278-4f23-81ac-523370b3245e&maxDataAge=3600&theme=light&autoRefresh=true"
             }
          />
       </div>
